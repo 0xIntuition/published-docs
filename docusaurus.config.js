0405c77151cb50f932abfa2bd6c9ddcc42b1a57d
0xbentobyte <kelly@intuition.systems>
2024-09-22 15:30:28 -0700

    Update docusaurus.config.js
    Signed-off-by: 0xbentobyte <kelly@intuition.systems>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "docu-notion test",
+  title: "Intuition Docs",
   tagline: "",
   url: "https://0xintuition.github.io",
   baseUrl: "/published-docs/",
