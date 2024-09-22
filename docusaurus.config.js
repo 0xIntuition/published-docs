1d4d1211fe9fe5c9505cd8a28dfde77d0bc4cdfe
0xbentobyte <kelly@intuition.systems>
2024-09-22 16:03:27 -0700

    Update docusaurus.config.js
    Signed-off-by: 0xbentobyte <kelly@intuition.systems>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+// const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -96,15 +96,10 @@ const config = {
           },
         ],
       },
-      colorMode: {
-        defaultMode: 'dark',
-        disableSwitch: true,
-        respectPrefersColorScheme: false,
+      prism: {
+        theme: darkCodeTheme,
+        darkTheme: darkCodeTheme,
       },
-      // prism: {
-      //  theme: lightCodeTheme,
-      //  darkTheme: darkCodeTheme,
-      // },
     }),
 };
 
