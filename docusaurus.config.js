201b372a1d6036f734796cd694b8dd67ed45f9bf
0xbentobyte <kelly@intuition.systems>
2024-10-03 16:35:06 -0700

    Update docusaurus.config.js
    Signed-off-by: 0xbentobyte <kelly@intuition.systems>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
       }),
     ],
   ],
-   stylesheets: [
+ /*  stylesheets: [
         {
           href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
           type: 'text/css',
           integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
           crossorigin: 'anonymous',
         },
-      ],
+      ],*/
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
