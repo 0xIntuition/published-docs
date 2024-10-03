dedbad8bafe268c81a6efacc35c61657b29f268c
0xbentobyte <kelly@intuition.systems>
2024-10-03 09:30:12 -0700

    Update docusaurus.config.js
    Signed-off-by: 0xbentobyte <kelly@intuition.systems>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,8 +31,8 @@ const config = {
         theme: {
           customCss: [
             require.resolve("./css/docu-notion-styles.css"),
-            require.resolve("./css/colors.css")
             require.resolve("./css/custom2.css"),
+            require.resolve("./css/colors.css")
             require.resolve(
               "./css/gifplayer.css"
               //"./node_modules/react-gif-player/dist/gifplayer.css" // this gave a big red compile warning which is seaming unrelated "  Replace Autoprefixer browsers option to Browserslist config..."
