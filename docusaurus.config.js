3b4953bd76483a0b3d1cc5388821a67e6d93b2e7
0xbentobyte <kelly@intuition.systems>
2024-10-03 15:39:26 -0700

    Update docusaurus.config.js
    Signed-off-by: 0xbentobyte <kelly@intuition.systems>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-/* const lightCodeTheme = require("prism-react-renderer/themes/github"); */
-/* const darkCodeTheme = require("prism-react-renderer/themes/vsDark"); */
+const lightCodeTheme = require("prism-react-renderer/themes/github"); 
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark"); 
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -100,7 +100,7 @@ const config = {
         title: "Intuition Docs",
         logo: {
           alt: "Logo",
-          src: "img/logo-bw.svg",
+          src: "img/intuition-logo-white.svg",
         },
         items: [
                     {
